0ee2520bf3d5c0d2562de4d6ec5d5b700fabb93d
Luke O'Sullivan <osulluke@gmail.com>
2019-11-30 21:17:47 -0700

    Commit 4.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+void function(void);
+
 int main(void) {
-    std::cout << "Hello world" << std::endl;
+    function();
 
     return 0;
+}
+
+void function(void) {
+    std::cout << "Hello world" << std::endl;
+
+    return;
 }
